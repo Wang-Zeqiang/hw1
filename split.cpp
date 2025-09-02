880552a9086234f23cbffd102b57a640a1f96135
Zeqiang Wang <zeqiang@usc.edu>
2025-09-02 19:36:16 +0000

    all done with part1

--- split.cpp
+++ split.cpp
@@ -14,10 +14,48 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
+Node* Find_Tail(Node* in);
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  if(in == nullptr){return;}
+
+  Node* curr_ptr = in;
+  int curr_val = curr_ptr->value;
+  // move in-list 1 forward before change the curr next to NULL
+  in = in->next; 
+  curr_ptr->next = nullptr; // change node from in-list to odds/evens
+
+  if(curr_val % 2 == 0){
+    if(evens == nullptr){
+      evens = curr_ptr;
+    }
+    else{
+      Node* tail = Find_Tail(evens);
+      tail->next = curr_ptr;
+    }
+  }
+  else{
+    if(odds == nullptr){
+      odds = curr_ptr;
+    }
+    else{
+      Node* tail = Find_Tail(odds);
+      tail->next = curr_ptr;
+    }
+  }
+
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+Node* Find_Tail(Node* in){
+  if(in->next == nullptr){
+    return in;
+  }
+  in = in->next;
+  return Find_Tail(in);
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,51 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+void print(Node* head);
+void empty(Node* head);
 
 int main(int argc, char* argv[])
 {
+  Node* head = new Node(0, nullptr);;
+  Node* temp = head;
+  for(int i = 1; i < 15; i++){
+    temp->next = new Node(i, nullptr);
+    temp = temp->next;
+  }
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+  split(head, odds, evens);
 
+  print(odds);
+  print(evens);  
+  empty(odds);
+  empty(evens);
+  empty(head);
+  
+}
+
+void print(Node* head){
+  Node* temp = head;
+  while(temp != nullptr){
+    cout << temp->value << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+
+void empty(Node* head){
+  Node * currElement = head;
+
+	while(currElement != nullptr)
+	{
+		Node * nextElement = currElement->next;
+
+		delete currElement;
+
+		currElement = nextElement;
+	}
 }
